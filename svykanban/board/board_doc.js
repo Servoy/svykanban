54c87ff674cc6ae3c01e876bf8b80bb9fb40a7d4
Danny Camargo <dcamargo@servoy.com>
2023-09-19 14:04:20 -0400

    Add documentation

--- svykanban/board/board_doc.js
+++ svykanban/board/board_doc.js
@@ -1,13 +1,31 @@
 
-
+/** 
+ * Adds all the boards to be displayed by the component,
+ * any previous boards will be replaced by the new ones
+ * 
+ * @param {Array<CustomType<svykanban-board.boardItem>>} boards
+ */
 function addBoards(boards) {
 
 }
 
+/**
+ * Adds an item to a specific Board
+ * 
+ * @param {String} bid the Board Id, if not found the item will be ignored
+ * @param {CustomType<svykanban-board.item>} el the item to be added to the Board
+ * @param {number} [position] the position to insert the new item at, if not provided it will be inserted at the end
+ */
 function addElement(bid, el, position) {
 
 }
 
+/**
+ * Updates the title of an existing item
+ * 
+ * @param {String} bid the Board Id, if not found the item will be ignored
+ * @param {CustomType<svykanban-board.item>} el the item to be updated, the title of the existing item will be replaced with this one, if not found by id it will be ignored
+ */
 function updateElement(bid, el) {
 
 }
