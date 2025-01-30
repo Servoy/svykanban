ed8bfa935e0a59813e02422f509b17b1c5f52358
mvid <mvid@servoy.com>
2025-01-30 18:37:08 +0200

    SVY-19830 Review and update all _doc.js files from documented plugins

--- svykanban/board/board_doc.js
+++ svykanban/board/board_doc.js
@@ -3,7 +3,7 @@
  * Adds all the boards to be displayed by the component,
  * any previous boards will be replaced by the new ones
  * 
- * @param {Array<CustomType<svykanban-board.boardItem>>} boards
+ * @param {Array<CustomType<svykanban-board.boardItem>>} boardItems An array of board items to be displayed, replacing any existing boards.
  */
 function addBoards(boards) {
 
@@ -12,20 +12,20 @@ function addBoards(boards) {
 /**
  * Adds an item to a specific Board
  * 
- * @param {String} bid the Board Id, if not found the item will be ignored
- * @param {CustomType<svykanban-board.item>} el the item to be added to the Board
+ * @param {String} boardID the Board Id, if not found the item will be ignored
+ * @param {CustomType<svykanban-board.item>} element the item to be added to the Board
  * @param {number} [position] the position to insert the new item at, if not provided it will be inserted at the end
  */
-function addElement(bid, el, position) {
+function addElement(boardID, element, position) {
 
 }
 
 /**
  * Updates the title of an existing item
  * 
- * @param {String} bid the Board Id, if not found the item will be ignored
- * @param {CustomType<svykanban-board.item>} el the item to be updated, the title of the existing item will be replaced with this one, if not found by id it will be ignored
+ * @param {String} elementID the Board Id, if not found the item will be ignored
+ * @param {CustomType<svykanban-board.item>} element the item to be updated, the title of the existing item will be replaced with this one, if not found by id it will be ignored
  */
-function updateElement(bid, el) {
+function updateElement(elementID, element) {
 
 }
